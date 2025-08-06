3cd353ae31916fd2057aec62d6dbfc88e5a6d8db
Quiroz-Zuniga <elquiroz08@gmail.com>
2025-08-05 18:14:35 -0600

    Create vite

---
+++ src/main.js
@@ -0,0 +1,43 @@
+const dia = document.getElementById("dia");
+const mes = document.getElementById("mes");
+const boton = document.getElementById("calcular");
+const resultado = document.getElementById("resultado");
+const fechahoy = document.querySelector(".date");
+
+
+
+boton.addEventListener("click", function() {
+    const diaCumple = parseInt(dia.value);
+    const mesCumple = parseInt(mes.value);
+    diasHastaCumpleaños(diaCumple, mesCumple);
+})
+
+fechahoy.textContent = new Date().toLocaleDateString();
+function diasHastaCumpleaños(dia, mes) {
+    const date = new Date();
+    const añoActual = date.getFullYear();
+    const cumpleaños = new Date(añoActual, mes - 1, dia);
+    if (date > cumpleaños) {
+        const proximoAño = añoActual + 1;
+        cumpleaños.setFullYear(proximoAño);
+    }
+  
+    const diferenciaDias = Math.ceil((cumpleaños - date) / (1000 * 60 * 60 * 24));
+   if (
+        date.getDate() === cumpleaños.getDate() &&
+        date.getMonth() === cumpleaños.getMonth()
+    ) {
+        resultado.style.display = "block";
+        resultado.textContent = "¡Feliz Cumpleaños!";
+        return;
+    }else if (diferenciaDias == 1) {
+        resultado.style.display = "block";
+        resultado.textContent = "!Mañana es tu cumpleaños!";
+        return;
+    }else{
+        resultado.style.display = "block";
+        resultado.textContent = `Faltan ${diferenciaDias} días para tu cumpleaños`;
+        return;
+    }
+}
+
